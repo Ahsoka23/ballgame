c54e31fc9a16250212b5fb1be85b1c4b7e19e445
Ahsoka23 <89179461+Ahsoka23@users.noreply.github.com>
2022-09-08 19:34:05 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+
+
+function setup(){
+ 
+}
+
+function draw(){
+
+}
+
+function writePosition(x,y){
+
+}
+
+function readPosition(data){
+
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
